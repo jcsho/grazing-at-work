f29799905f0436962341f9d173a96fbac80d9a30
Justin Ho <github@jcsh.dev>
2021-01-18 17:32:12 -0800

    feat: add landing page

---
+++ components/Layout.tsx
@@ -0,0 +1,23 @@
+import React, { ReactNode } from "react";
+import Head from "next/head";
+
+type Props = {
+  children?: ReactNode;
+  title?: string;
+};
+
+const Layout = ({ children, title }: Props) => (
+  <div>
+    <Head>
+      <title>{title}</title>
+      <meta charSet="utf-8" />
+      <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+    </Head>
+    {children}
+    <footer>
+      <p>&#169; Justin Ho 2021</p>
+    </footer>
+  </div>
+);
+
+export default Layout;
---
+++ pages/index.tsx
@@ -0,0 +1,7 @@
+import Layout from "../components/Layout";
+
+const IndexPage = () => (
+  <Layout title="Hashnode Vercel Hackathon Submission - Justin"></Layout>
+);
+
+export default IndexPage;
