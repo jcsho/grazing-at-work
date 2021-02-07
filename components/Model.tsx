86cc01ed9e3986c316c0585f1c9045f7636c48bc
Justin Ho <github@jcsh.dev>
2021-02-07 13:55:32 -0800

    refactor(model): use enums for animations and state

--- components/Model.tsx
+++ components/Model.tsx
@@ -1,31 +1,71 @@
 import { useAnimations, useGLTF } from "@react-three/drei";
-import { useEffect, useState } from "react";
+import { Dispatch, SetStateAction, useEffect, useState } from "react";
 import { useFrame } from "react-three-fiber";
 import { Mesh, SkinnedMesh, Vector3 } from "three";
 
-interface ModelProps {
-  modelPosition: Vector3;
+export enum ModelState {
+  Idle,
+  Moving,
 }
 
-const Model: React.FC<ModelProps> = ({ modelPosition }) => {
-  const { nodes, materials, animations } = useGLTF("/glb/ox.glb");
-  const { ref, actions, names } = useAnimations(animations);
+// Animations from GLB file
+// To see the full list, log
+// the `actions` value from useAnimations()
+enum Actions {
+  Bounce = "Bounce",
+  Clicked = "Clicked",
+  Death = "Death",
+  Idle = "Idle",
+  Jump = "Jump",
+  Munch = "Munch",
+  Roll = "Roll",
+  Run = "Run",
+  Spin = "Spin",
+  Walk = "Walk",
+}
 
-  const [index, setIndex] = useState<number>(3);
+interface ModelProps {
+  modelPosition: Vector3;
+  modelState: ModelState;
+  setModelState: Dispatch<SetStateAction<ModelState>>;
+}
+
+const Model: React.FC<ModelProps> = ({
+  modelPosition,
+  modelState,
+  setModelState,
+}) => {
+  const { nodes, materials, animations } = useGLTF("/glb/ox.glb");
+  const { ref, actions } = useAnimations(animations);
+
+  const [action, setAction] = useState<Actions>(Actions.Idle);
 
   /* Change Animations */
   useEffect(() => {
-    actions[names[index]].reset().fadeIn(0.1).play();
-    return () => actions[names[index]].fadeOut(0.1);
-  }, [actions, names, index]);
+    actions[action].reset().fadeIn(0.1).play();
+    return () => actions[action].fadeOut(0.1);
+  }, [actions, action]);
+
+  /* Change State */
+  useEffect(() => {
+    console.log(ModelState[modelState]);
+    switch (modelState) {
+      case ModelState.Idle:
+        setAction(Actions.Idle);
+        break;
+      case ModelState.Moving:
+        setAction(Actions.Run);
+        break;
+    }
+  }, [modelState]);
 
   useFrame(() => {
     ref.current.lookAt(modelPosition);
     if (ref.current.position.distanceTo(modelPosition) > 1) {
       ref.current.position.lerp(modelPosition, 0.05);
-      setIndex(7);
-    } else {
-      setIndex(3);
+      setModelState(ModelState.Moving);
+    } else if (modelState === ModelState.Moving) {
+      setModelState(ModelState.Idle);
     }
   });
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -3,20 +3,25 @@ import { Canvas } from "react-three-fiber";
 import { Vector3 } from "three";
 import Layout from "../components/Layout";
 import Lights from "../components/Lights";
-import Model from "../components/Model";
+import Model, { ModelState } from "../components/Model";
 import Terrain from "../components/Terrain";
 
 const Index = () => {
   const [modelPosition, setModelPosition] = useState<Vector3>(
     new Vector3(0, 0, 0)
   );
+  const [modelState, setModelState] = useState<ModelState>(ModelState.Idle);
 
   return (
     <Layout title="Hashnode Vercel Hackathon Submission - Justin">
       <Canvas colorManagement camera={{ position: [5, 5, 5] }}>
         <Lights />
         <Suspense fallback={null}>
-          <Model modelPosition={modelPosition} />
+          <Model
+            modelPosition={modelPosition}
+            modelState={modelState}
+            setModelState={setModelState}
+          />
         </Suspense>
         <Terrain setModelPosition={setModelPosition} />
       </Canvas>
