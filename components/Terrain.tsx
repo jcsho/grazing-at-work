34a908650764b5a08aacda97f56bf1eb1b0cd20b
Justin Ho <github@jcsh.dev>
2021-02-13 00:22:10 -0800

    feat(terrain): import blender model with materials

--- components/Terrain.tsx
+++ components/Terrain.tsx
@@ -1,38 +1,41 @@
-import { Dispatch, SetStateAction, useRef } from "react";
-import { Intersection, Vector3 } from "three";
+import { useGLTF } from "@react-three/drei";
+import { Dispatch, SetStateAction, useEffect, useRef } from "react";
+import { Intersection, Mesh, MeshStandardMaterial, Vector3 } from "three";
 
 interface TerrainProps {
   setModelPosition: Dispatch<SetStateAction<Vector3>>;
 }
 
 const Terrain: React.FC<TerrainProps> = ({ setModelPosition }) => {
-  const mesh = useRef();
+  const { nodes, materials } = useGLTF("/terrain-valley.glb");
 
   const handleOnClick = (intersection: Intersection[]) => {
     if (intersection.length > 0) {
-      // rotated mesh has an off axis y-value
-      // but x and z values are correct somehow
-      // so setting to 0 (ground plane value) is better
-      setModelPosition(intersection[0].point.setY(0));
+      setModelPosition(intersection[0].point);
     }
   };
 
   return (
-    <mesh
-      visible
-      position={[0, 0, 0]}
-      rotation={[-Math.PI / 2, 0, 0]}
-      ref={mesh}
-      onClick={(e) => handleOnClick(e.intersections)}
-    >
-      <planeBufferGeometry attach="geometry" args={[10, 10, 10, 10]} />
-      <meshStandardMaterial
-        attach="material"
-        color="green"
-        roughness={1}
-        metalness={0}
-      />
-    </mesh>
+    <>
+      <group dispose={null}>
+        <mesh
+          castShadow
+          receiveShadow
+          onClick={(e) => handleOnClick(e.intersections)}
+          position={[0, 4, 0]}
+          geometry={(nodes["terrain-valley"] as Mesh).geometry}
+          material={materials["36 GREEN"]}
+        ></mesh>
+        <mesh
+          castShadow
+          receiveShadow
+          position={[0, -0.7, 0]}
+          scale={[2.97, 1.37, 1]}
+          geometry={(nodes["hexagon-water_1"] as Mesh).geometry}
+          material={materials["48 TRANSPARENT BLUE"]}
+        />
+      </group>
+    </>
   );
 };
 
