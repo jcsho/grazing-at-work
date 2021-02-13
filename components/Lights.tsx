9422d90bceec79642b0739bcb801cd05802dcb05
Justin Ho <github@jcsh.dev>
2021-02-13 00:22:38 -0800

    feat(lights): change to 3-point lighting with night scene

--- components/Lights.tsx
+++ components/Lights.tsx
@@ -3,21 +3,10 @@ import React from "react";
 const Lights = () => {
   return (
     <>
-      <ambientLight intensity={0.1} />
-      <directionalLight position={[40, 10, 5]} intensity={0.2} />
-      <directionalLight
-        castShadow
-        position={[10, 420, 100]}
-        intensity={1.3}
-        shadow-mapSize-width={1024}
-        shadow-mapSize-height={1024}
-        shadow-camera-far={10}
-        shadow-camera-left={-30}
-        shadow-camera-right={10}
-        shadow-camera-top={40}
-        shadow-camera-bottom={-10}
-      />
-      <spotLight intensity={0.5} position={[90, 100, 50]} castShadow />
+      <spotLight position={[60, 30, 60]} intensity={1} distance={250} />
+      <spotLight position={[60, 30, -60]} intensity={0.5} distance={250} />
+      <directionalLight position={[-200, 90, 75]} intensity={0.2} />
+      <directionalLight position={[0, -50, 0]} intensity={0.5} />
     </>
   );
 };
