ef1fc67a300065b883922e9d87efa0daa18e9a17
Justin Ho <github@jcsh.dev>
2021-02-18 14:02:11 -0800

    refactor: icons into separate component, css modules

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,19 +1,27 @@
 import Image from "next/image";
+import Icon from "./Icon";
 
-const Footer = () => (
-  <>
-    <div className="overlay footer">
-      <div className="footer__icon">
-        <Image src="/bookmark.svg" alt="Licenses" width={35} height={35} />
+const Footer = () => {
+  return (
+    <>
+      <div className="overlay footer">
+        <Icon
+          imageUrl="/bookmark.svg"
+          altText="Licenses"
+          iconSize={35}
+          hoverText="Licenses"
+        />
+        <a href="https://github.com/justinhodev/grazing-at-work">
+          <Icon
+            imageUrl="/github.svg"
+            altText="Source Code"
+            iconSize={50}
+            hoverText="Source Code"
+          />
+        </a>
       </div>
-      <a
-        href="https://github.com/justinhodev/grazing-at-work"
-        className="footer__icon"
-      >
-        <Image src="/github.svg" alt="Source Code" width={50} height={50} />
-      </a>
-    </div>
-  </>
-);
+    </>
+  );
+};
 
 export default Footer;
---
+++ components/Icon.tsx
@@ -0,0 +1,41 @@
+import Image from "next/image";
+import { useState } from "react";
+
+import styles from "../styles/Icon.module.css";
+
+interface IconProps {
+  imageUrl: string;
+  altText: string;
+  hoverText: string;
+  iconSize: number;
+}
+
+const Icon: React.FC<IconProps> = ({
+  imageUrl,
+  altText,
+  hoverText,
+  iconSize,
+}) => {
+  const [hover, setHover] = useState(false);
+
+  return (
+    <div
+      className={styles.icon}
+      onMouseEnter={() => setHover(true)}
+      onMouseLeave={() => setHover(false)}
+    >
+      {hover ? (
+        hoverText
+      ) : (
+        <Image
+          src={imageUrl}
+          alt={altText}
+          width={iconSize}
+          height={iconSize}
+        />
+      )}
+    </div>
+  );
+};
+
+export default Icon;
